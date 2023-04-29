a66040c75a9307270aeca524ca898de3b56f4bc6
Joseph N. Moturi <josemoturi17@gmail.com>
2023-04-29 04:06:10 +0300

    Added some web files like  HTML, CSS, Javascript

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,22 @@
 import streamlit as st
 
-st.write('Hello World!')
+import streamlit as st
+
+# Define the app route
+@app.route('/')
+def index():
+    return render_template('index.html', name='John')
+
+# Define the Streamlit app
+def app():
+    # Set the page title and favicon
+    st.set_page_config(page_title="My Streamlit App", page_icon=":guardsman:")
+
+    # Define the app layout
+    st.write("Welcome to my app!")
+    st.write("This is the home page.")
+    name = st.text_input("Enter your name:")
+    st.write(f"Hello, {name}!")
+
+if __name__ == '__main__':
+    app.run(debug=True)
