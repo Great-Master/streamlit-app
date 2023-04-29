4f5c8a3731e73818ef0345b581c337e740e1dbd6
Joseph N. Moturi <josemoturi17@gmail.com>
2023-04-29 04:35:44 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,19 @@
 import streamlit as st
 
 # Define the Streamlit app
-def app():
-    # Set the page title and favicon
-    st.set_page_config(page_title="My Streamlit App", page_icon=":guardsman:")
-
+def main():
+    # Read the contents of the HTML file
+    with open("index.html", "r") as f:
+        page = f.read()
+    
     # Define the app layout
     st.write("Welcome to my app!")
     st.write("This is the home page.")
     name = st.text_input("Enter your name:")
     st.write(f"Hello, {name}!")
 
+    # Display the contents of the HTML file on Streamlit
+    st.write(page, unsafe_allow_html=True)
+
 if __name__ == '__main__':
-    app()
+    main()
